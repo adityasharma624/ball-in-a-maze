79ffc936dfb73212e7fb7f3bbed49280f8f7434f
Aditya Sharma <adityas.cse@gmail.com>
2025-06-30 15:53:16 +0530

    added ball movement

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float speed;
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0.0f, verticalInput) * speed * Time.deltaTime;
+
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.AddForce(movement, ForceMode.Impulse);
+    }
+}
